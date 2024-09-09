0474fe581eb63586c4e492752c9ff62dbb41557b
Aifa_P <aifamajeed75@gmail.com>
2024-09-10 03:14:38 +0530

    modify logger

--- utils/logger.js
+++ utils/logger.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: "artistry" },
   transports: [
-    new winston.transports.File({ filename: "error-log", level: "error" }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
 });
